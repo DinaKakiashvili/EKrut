5710e7511e4e0d6f6d0bb678973f47f361b05455
dina <dinaka53@gmail.com>
2022-12-14 22:19:34 +0200

    g

--- EKrut_Client/src/gui/ClientUI.java
+++ EKrut_Client/src/gui/ClientUI.java
@@ -16,7 +16,7 @@ public class ClientUI extends Application
     	ClientOpeningScreenController cl = new ClientOpeningScreenController();
     	System.out.println("natalie and dina");
     	System.out.println("check");
-    	System.out.println("sheker kolshehu");
+    	System.out.println("sheker kolshehuuu");
         cl.start(primaryStage);
         
     }
