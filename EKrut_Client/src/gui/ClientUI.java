f2a22a1563787da32f65ec068b277e485d9366e9
dina <dinaka53@gmail.com>
2022-12-14 15:27:42 +0200

    start

--- EKrut_Client/src/gui/ClientUI.java
+++ EKrut_Client/src/gui/ClientUI.java
@@ -14,7 +14,7 @@ public class ClientUI extends Application
     
     public void start(final Stage primaryStage) throws Exception {
     	ClientOpeningScreenController cl = new ClientOpeningScreenController();
-    	System.out.println("startttt");
+    	System.out.println("starttt");
         cl.start(primaryStage);
         
     }
