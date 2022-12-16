f994993831f3958c3670a48ee599b55973ec4267
dina <dinaka53@gmail.com>
2022-12-16 14:27:18 +0200

    password field

--- EKrut_Client/src/gui/ClientLoginScreenController.java
+++ EKrut_Client/src/gui/ClientLoginScreenController.java
@@ -11,6 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -25,7 +26,7 @@ public class ClientLoginScreenController implements EventHandler<WindowEvent> {
     private Button userLogin;
 
     @FXML
-    private TextField passwordTxt;
+    private PasswordField passwordTxt;
     
     @FXML
     private Label errorLabel;
