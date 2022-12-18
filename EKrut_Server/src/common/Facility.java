f8bc24f1188456e7fb6212fe95828608cdb67d7f
maayannachmias <maayansharvit1997@gmail.com>
2022-12-18 10:05:32 +0200

    new Enum added (ekrut-facilities)

---
+++ EKrut_Server/src/common/Facility.java
@@ -0,0 +1,15 @@
+package common;
+
+public enum Facility {
+	EILAT("Eilat", 0),
+	RAMAT_GAN("Ramat Gan", 1),
+	KIRYAT_MOTZKIN("Kiryat Motzkin", 2), 
+	TZFAT("Tzfat", 3),
+	TIRAT_HACARMEL("Tirat Hacarmel", 4), 
+	BEER_SHEVA("Be'er Sheva", 5), 
+	TEL_AVIV("Tel Aviv", 6),
+	WAREHOUSE("Warehouse", 7);
+
+	private Facility(final String facility, final int serialNumber) {
+	}
+}
